cd844831f40708659ea269860d41d8948c1012c4
Shibam802 <shibambanik0@gmail.com>
2025-10-24 15:03:32 +0530

    speration of concerns

---
+++ models/owners-model.js
@@ -0,0 +1,23 @@
+const mongoose= require('mongoose');
+
+
+
+const ownerSchema= mongoose.Schema({
+    fullname:{
+        type:String,
+        minLength:3,
+        trim:3,
+    },
+    email: String,
+    password:String,
+   
+    
+    products:{
+        type:Array,
+        default:[]
+    },
+   gstin:String,
+    picture:String,
+});
+
+module.exports=mongoose.model("owner",ownerSchema);
